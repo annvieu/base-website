5489fdd1fdfd07c2a02002be207a0095c4eb7b3b
annvieu <annkurochkina000@ya.ru>
2026-03-04 23:13:14 +0300

    fix: change date

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -4,10 +4,10 @@ export class AppFooter extends HTMLElement {
             <footer class="footer">
                 <div class="grid-container">
                     <a href="https://t.me/nobases" class="f-1"><img src="/icons/tg-icon.svg" width="47"></a>
-                    <a href="#" class="f-2">связаться</a>
+                    <a href="https://t.me/nobases" class="f-2">связаться</a>
                     <a href="#" class="f-3">о проекте</a>
                     <a href="#" class="f-8">правовая информация</a>
-                    <p class="f-10">© no base 2025</p>
+                    <p class="f-10">© no base 2026</p>
                 </div>
             </footer>
         `;
