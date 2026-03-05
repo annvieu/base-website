64ccb7d215be0921da228b44979a6583d2723517
annvieu <annkurochkina000@ya.ru>
2026-03-05 06:08:07 +0300

    init: add about page

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -1,3 +1,5 @@
+import { getPath } from '../main.js';
+
 export class AppFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -5,7 +7,7 @@ export class AppFooter extends HTMLElement {
                 <div class="grid-container">
                     <a href="https://t.me/nobases" class="f-1"><img src="/icons/tg-icon.svg" width="47"></a>
                     <a href="https://t.me/nobases" class="f-2">связаться</a>
-                    <a href="#" class="f-3">о проекте</a>
+                    <a href="${getPath('pages/about-article.html')}" class="f-3">о проекте</a>
                     <a href="#" class="f-8">правовая информация</a>
                     <p class="f-10">© no base 2026</p>
                 </div>
