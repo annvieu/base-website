323666a364413866dc9213b70784aea830e3e48e
annvieu <annkurochkina000@ya.ru>
2026-03-05 06:11:26 +0300

    hot-fix: repair path to tg icon

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -5,7 +5,7 @@ export class AppFooter extends HTMLElement {
         this.innerHTML = `
             <footer class="footer">
                 <div class="grid-container">
-                    <a href="https://t.me/nobases" class="f-1"><img src="/icons/tg-icon.svg" width="47"></a>
+                    <a href="https://t.me/nobases" class="f-1"><img src="${getPath('icons/tg-icon.svg')}" width="47"></a>
                     <a href="https://t.me/nobases" class="f-2">связаться</a>
                     <a href="${getPath('pages/about-article.html')}" class="f-3">о проекте</a>
                     <a href="#" class="f-8">правовая информация</a>
